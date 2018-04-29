955ec048c6a8f55f2d92bc7c1cc0fcac02fe0d28
Sabbir Hossain <37515091+sabbirMhossain@users.noreply.github.com>
2018-04-29 19:24:21 -0400

    Delete getFacebook.java

--- Facebook/src/main/java/mainftp/getFacebook.java
+++
@@ -1,9 +0,0 @@
-package mainftp;
-
-import base.CommonAPI;
-
-public class getFacebook extends CommonAPI {
-    public void getfb(){
-        driver.navigate().to("http://www.facebook.com/");
-    }
-}
