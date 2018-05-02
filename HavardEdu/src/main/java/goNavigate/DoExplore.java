db1aa00c3562204eacf2fb05122be65a7e56db22
mrahman77 <mikatmahmud@gmail.com>
2018-05-02 18:54:12 -0400

    IndentationOfCode

--- HavardEdu/src/main/java/goNavigate/DoExplore.java
+++ HavardEdu/src/main/java/goNavigate/DoExplore.java
@@ -11,37 +11,23 @@ import org.openqa.selenium.WebElement;
  */
 public class DoExplore extends CommonAPI {
     public void playArround() throws InterruptedException {
-        //driver.navigate().to("https://www.harvard.edu/");
-
         clickByXpath("//a[text()='Contact Harvard']");
-//        sleepFor(5);
         navigateBack();
-//        sleepFor(5);
+        driver.get("https://youtu.be/Oi9LBSYZgEk"); // open browser at youtube video.
 
-        // /////////////////// /////////////// /////////////// //////////// /////////////
-
-        driver.get("https://youtu.be/Oi9LBSYZgEk"); // open your browser at youtube video.
-
-        WebElement video = driver.findElement(By.id("movie_player"));// use the id of the video to find it
-
-//        video.click(); // play the video
-
-// implicite wait
-//code .....
+        WebElement video = driver.findElement(By.id("movie_player"));
+        // implicite wait
 sleepFor(6);
-        video.click(); // pause de video
-
+        video.click(); // pause the video
         driver.navigate().to("https://www.harvard.edu/");
-
         clickByXpath("//*[@id=\"siteNavigation\"]/div/div[3]/ul/li[4]/a");
-
         sleepFor(5);
-
         driver.get("https://news.harvard.edu/gazette/story/2018/04/chimamanda-ngozi-adichie-named-harvards-class-day-speaker/");
 sleepFor(5);
-
         JavascriptExecutor jsx = (JavascriptExecutor)driver;
         sleepFor(5);
+        jsx.executeScript("window.scrollBy(0,900)", "");
+        jsx.executeScript("window.scrollBy(0,900)", "");
         jsx.executeScript("window.scrollBy(0,900)", "");
         sleepFor(5);
 
