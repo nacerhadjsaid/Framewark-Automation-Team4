02db2621d4f5e63fdc33c9b78ae1cedc1a4828eb
Nacer Hadj-said <nacer.hadjsaid@gmail.com>
2018-05-03 20:55:53 -0400

    changes

--- WaImart/src/test/java/menu/DropDownMenu.java
+++ WaImart/src/test/java/menu/DropDownMenu.java
@@ -1,4 +1,19 @@
 package menu;
 
-public class DropDownMenu {
+import base.CommonAPI;
+import org.apache.xpath.SourceTree;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DropDownMenu extends CommonAPI{
+    @Test
+    public void menu(){
+        List<WebElement> menu = driver.findElements(By.cssSelector(".SearchDropdown-flyout"));
+        for(WebElement element: menu){
+            System.out.println(element.getText());
+        }
+    }
 }
