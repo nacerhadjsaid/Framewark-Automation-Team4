54dc53f104d5933f9300fdcc40a26b2e026a7c06
Tarek Hasan <tareqhasan1717@gmail.com>
2018-05-05 01:16:02 -0400

    boa part

---
+++ BankOfAmerica/src/test/java/homePageTest/logInTest.java
@@ -0,0 +1,14 @@
+package homePageTest;
+
+import homePage.logIn;
+
+public class logInTest extends logIn {
+
+    public void logInBankOfAmerica(){
+        logeEle();
+
+    }
+    public void logInBankOfAmericaSe(){
+        tryLogIn();
+    }
+}
