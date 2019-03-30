de69e90c0a94f814784bf227e0f5c890fa040476
Nacer Hadj-said <nacer.hadjsaid@gmail.com>
2019-03-30 13:32:16 -0500

    changes

--- HB0/src/test/java/testdropdown/TestDropDown.java
+++ HB0/src/test/java/testdropdown/TestDropDown.java
@@ -1,4 +1,10 @@
 package testdropdown;
 
-public class TestDropDown {
+import org.testng.annotations.Test;
+
+public class TestDropDown extends DropDown{
+    @Test
+    public void testDropDown() throws InterruptedException{
+        dropDownmMnu();
+    }
 }
